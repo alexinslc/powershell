f6da4d52111ea6d3d8c535750cacb77fe43e82a1
Alex Lutz <alutz@netdocuments.com>
2014-05-21 07:52:01 -0600

    Created script to pull data from SQL.

---
+++ Get-SQL.ps1
@@ -0,0 +1,44 @@
+﻿<#
+This script allows you to query data from a Microsoft SQL server and returns the query as a System.Data.DataTable Object in PowerShell.
+
+WARNING: This script allows SQL injection. You should NEVER expose this publically.
+
+Usage: Get-SQL -SQLQuery "SELECT * FROM TABLENAME" -SQLServer "SQL SERVER" -Database "DATABASE NAME" -Username "SQL USERNAME" -Password "SQL PASSWORD"
+
+#>
+
+function Get-SQL()
+{
+    param(
+        [Parameter(mandatory=$true)][string]$SQLQuery,
+        [parameter(mandatory=$true)][string]$SQLServer,
+        [parameter(mandatory=$true)][string]$Database,
+        [parameter(mandatory=$true)][string]$UserName,
+        [parameter(mandatory=$true)][string]$Password
+    )
+    try{
+        $connectionString = “Server=$SQLServer;Database=$Database;User=$UserName;Password=$Password;Integrated Security=False;”
+        $connection = New-Object System.Data.SqlClient.SqlConnection
+        $connection.ConnectionString = $connectionString
+
+
+        
+        $connection.Open()
+        $command = $connection.CreateCommand()
+        $command.CommandText = $SQLQuery
+        $data = $command.ExecuteReader()
+        $result = New-Object System.Data.DataTable
+        $result.Load($data)
+        
+        if ($result.Rows.Count -eq 0){throw "Query:`n $query `n`nReturned no results."}
+        
+        $connection.Close()               
+        return $result
+    }
+    catch
+    {
+        $connection.Close()
+        Write-Warning $_
+        return $null
+    }
+}
